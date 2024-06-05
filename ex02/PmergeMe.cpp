52409d7682e8a44651cf938a8024e11d4312cd9e
Youness Amzil <amzilyouness@gmail.com>
2024-06-05 20:35:37 +0100

    Starting...

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************************** */
+/*																						  */
+/*                                                						d8b		888       */
+/*                                                  					Y8P		888       */
+/*                                                      						888       */
+/*		888  888	  .d88b.   	8888b.  	88888b.d88b.	88888888	888 	888       */
+/*		888  888 	d88""88b     	"88b 	888 "888 "88b    	d88P 	888 	888       */
+/*		888  888 	888  888 	.d888888 	888  888  888		d88P   	888 	888       */
+/*		Y88b 888 	Y88..88P 	888  888 	888  888  888	  d88P    	888 	888       */
+/*		"Y88888  	"Y88P"  	"Y888888 	888  888  888	88888888 	888 	888       */
+/*	  		 888																          */
+/*		Y8b d88P																          */
+/*		"Y88P"									By: yoamzil <yoamzil@student.1337.ma>     */
+/*																						  */
+/* ************************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe()
+{
+	// std::cout << "PmergeMe constructor called" << std::endl;
+}
+
+PmergeMe::~PmergeMe()
+{
+	// std::cout << "PmergeMe destructor called" << std::endl;
+}
+
+PmergeMe::PmergeMe(PmergeMe const &original)
+{
+	// std::cout << "PmergeMe copy constructor called" << std::endl;
+	*this = original;
+}
+
+PmergeMe &PmergeMe::operator=(PmergeMe const &original)
+{
+	if (this != &original)
+	{
+		// std::cout << "PmergeMe assignation operator called" << std::endl;
+		// database = original.database;
+	}
+	return (*this);
+}
